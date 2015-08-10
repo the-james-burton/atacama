dd1855a0a0e9909e886435643d00b2cda0c10e20
the-james-burton <the.james.burton@gmail.com>
2015-08-10 16:26:48 +0100

    initial rest api integration to server

--- app/scripts/controllers/ohlc.js
+++ app/scripts/controllers/ohlc.js
@@ -8,14 +8,60 @@
  * Controller of the atacamaApp
  */
 angular.module('atacamaApp')
-    .controller('OhlcCtrl', function($scope, tickService) {
+    .controller('OhlcCtrl', function($scope, $stomp, $resource, tickService) {
         console.log('OhlcCtrl has been created');
+
+        var url = 'http://localhost:48002';
 
         var sod = moment(0, "HH").format("x");
 
+        // {"date": 1437583864374, "open": 100.0, "high": 100.24021489109903, "low": 98.2724267098159, "close": 99.51909089116204, "volume": 107.79215866544341, "symbol": "ABC.L", "exchange": "FTSE100", "timestamp": "2015-07-22T17:52:04.377+01:00" }
+
         // $scope.data = tickService.list();
-        tickService.getTicksAfter('ABC', sod);
-        $scope.data = $scope.ticks;
+        var symbol = 'ABC';
+
+        //$scope.ticks = {};
+        //$scope.ticks[symbol] = [];
+        //$scope.ticks[symbol].values = [];
+
+        //$scope.ticks = [];
+
+        $stomp
+            .connect(url + '/ticks', [])
+
+        // frame = CONNECTED headers
+        .then(function(frame) {
+            console.log('connected to tick websocket');
+            var subscription = $stomp.subscribe('/topic/ticks', function(payload, headers, res) {
+                // alert(payload.message);
+                $scope.data[0].values.push(payload);
+                $scope.$apply();
+            }, {
+                "headers": "are awesome"
+            });
+
+        });
+
+        var GetTicksAfter = $resource(url + '/tick/:symbol/:date');
+
+        $scope.data = [{
+            key: symbol
+            //values: [{}]
+        }];
+
+        var ticks = GetTicksAfter.get({
+                    symbol: symbol,
+                    date: sod
+            }
+            ,function(response) {
+                $scope.data[0].values = response.ticks;
+            }
+        );
+
+
+        //tickService.getTicksAfter(symbol, sod, $scope.data[0].values);
+        //$scope.ticks[symbol].values = response.ticks;
+        //$scope.data.values = $scope.ticks.ticks;
 
         $scope.options = {
             chart: {
--- app/scripts/services/tickservice.js
+++ app/scripts/services/tickservice.js
@@ -8,50 +8,32 @@
  * Service in the atacamaApp.
  */
 angular.module('atacamaApp')
-    .service('tickService', function($rootScope, $stomp, $resource) {
+    .service('tickService', function($rootScope, $resource) {
         // AngularJS will instantiate a singleton by calling "new" on this function
         console.log('tickService has been created');
 
         var url = 'http://localhost:48002';
 
-        $stomp
-            .connect(url + '/ticks', [])
+        // TODO 1. return from resource to passed-in variable $scope.data[0].values does not work
+        // TODO 2. cannot access $scope.data[0].values from websocket callback in here
 
-        // frame = CONNECTED headers
-        .then(function(frame) {
-            console.log('connected to tick websocket');
-            var subscription = $stomp.subscribe('/topic/ticks', function(payload, headers, res) {
-                // alert(payload.message);
-                $rootScope.ticks[0].values.push(payload);
-                $rootScope.$apply();
-            }, {
-                "headers": "are awesome"
-            });
+        //this.list = function() {
+        //    return $rootScope.ticks;
+        //};
 
-        });
+        // var GetTicksAfter = $resource(url + '/tick/:symbol/:date');
 
+        //this.getTicksAfter = function(symbol, date, reply) {
+        //    var call = GetTicksAfter.get({
+        //            symbol: symbol,
+        //            date: date
+        //        }
+        //        ,function(response) {
+        //            reply = response.ticks;
+        //        }
+        //    )
+        //};
 
-        // {"date": 1437583864374, "open": 100.0, "high": 100.24021489109903, "low": 98.2724267098159, "close": 99.51909089116204, "volume": 107.79215866544341, "symbol": "ABC.L", "exchange": "FTSE100", "timestamp": "2015-07-22T17:52:04.377+01:00" }
-        $rootScope.ticks = [{
-            key: "Stock Price",
-            values: []
-        }];
-
-        this.list = function() {
-            return $rootScope.ticks;
-        };
-
-        var GetTicksAfter = $resource(url + '/tick/:symbol/:date');
-
-        this.getTicksAfter = function(symbol, date) {
-            var response = GetTicksAfter.get({
-                    symbol: 'ABC',
-                    date: date
-                },
-                function(ticks) {
-                    $rootScope.ticks[0].values = response.ticks;
-                })
-        };
 
         //this.getTicksAfter(symbol, date) {
         //    $http.get(url + '/' + symbol + '/' + date).
