a8c64b0f1490f5ffe9caff4546245ad06ea06bd7
the-james-burton <the.james.burton@gmail.com>
2015-08-10 16:27:15 +0100

    added deps: lodash, restangular

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -40,6 +40,8 @@ module.exports = function(config) {
       'bower_components/ng-stomp/ng-stomp.js',
       'bower_components/moment/moment.js',
       'bower_components/angular-moment/angular-moment.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/restangular/dist/restangular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
