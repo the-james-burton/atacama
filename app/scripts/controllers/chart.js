14b91bd211d5e614b12b407e2f4ec05b9a528db3
the-james-burton <the.james.burton@gmail.com>
2015-10-22 17:32:01 +0100

    chart now uses stock indicators

--- app/scripts/controllers/chart.js
+++ app/scripts/controllers/chart.js
@@ -15,16 +15,13 @@ angular.module('atacamaApp')
 
     var sod = moment(0, "HH").format("x");
 
-    // {"date": 1437583864374, "open": 100.0, "high": 100.24021489109903, "low": 98.2724267098159, "close": 99.51909089116204, "volume": 107.79215866544341, "symbol": "ABC.L", "market": "FTSE100", "timestamp": "2015-07-22T17:52:04.377+01:00" }
-    // {"date":1445445787547,"closePriceIndicator":101.3170343070584,"bollingerBandsMiddleIndicator":101.83027737657682,"bollingerBandsLowerIndicator":100.43909562456751,"bollingerBandsUpperIndicator":103.22145912858613,"symbol":"DEF","market":"FTSE100","timestamp":"2015-10-21T17:43:07.547+01:00"}
-
     var market = 'FTSE100';
     var symbol = 'ABC';
 
     ngstomp
-      .subscribe('/topic/ticks.' + market + '.' + symbol, onTick, {}, $scope);
+      .subscribe('/topic/stocks.' + market + '.' + symbol, onMessage, {}, $scope);
 
-    function onTick(message) {
+    function onMessage(message) {
 
       var payload = JSON.parse(message.body);
       // how to merge arrays in lodash..
@@ -32,43 +29,46 @@ angular.module('atacamaApp')
 
       // we expect that each charted item is present in the incoming payload...
       _.forEach($scope.data, function(item) {
-        Array.prototype.push.apply($scope.data[item.position].values, _.map([payload], _.curry(convertTicks)(item.key)));
+        Array.prototype.push.apply($scope.data[item.position].values, _.map([payload], _.curry(convertMessages)(item.key)));
       });
 
       $scope.$apply();
     }
 
-    // position is a bespoke non-nvd3 field used in the onTick function above...
+    // {"date": 1437583864374, "open": 100.0, "high": 100.24021489109903, "low": 98.2724267098159, "close": 99.51909089116204, "volume": 107.79215866544341, "symbol": "ABC.L", "market": "FTSE100", "timestamp": "2015-07-22T17:52:04.377+01:00" }
+    // {"date":1445445787547,"closePriceIndicator":101.3170343070584,"bollingerBandsMiddleIndicator":101.83027737657682,"bollingerBandsLowerIndicator":100.43909562456751,"bollingerBandsUpperIndicator":103.22145912858613,"symbol":"DEF","market":"FTSE100","timestamp":"2015-10-21T17:43:07.547+01:00"}
+
+    // position is a bespoke non-nvd3 field used in the onMessage function above...
     $scope.data = [
           {
               values: [],
-              key: "open",
+              key: "closePriceIndicator",
               position: 0,
               color: "#bdc42d"
           },
           {
               values: [],
-              key: "high",
+              key: "bollingerBandsUpperIndicator",
               position: 1,
               color: "#2ca02c"
           },
           {
               values: [],
-              key: "low",
+              key: "bollingerBandsLowerIndicator",
               position: 2,
               color: "#9f442c"
           },
           {
               values: [],
-              key: "close",
+              key: "bollingerBandsMiddleIndicator",
               position: 3,
               color: "#2c649f"
           }
         ];
 
 
-    function convertTicks(property, ticks) {
-     return _(ticks).pick([property, 'date']).mapKeys(renameProperties).value();
+    function convertMessages(property, messages) {
+     return _(messages).pick([property, 'date']).mapKeys(renameProperties).value();
     }
 
     function renameProperties(value, key) {
@@ -81,12 +81,12 @@ angular.module('atacamaApp')
      return key;
    }
 
-    var ticks = Restangular.one('tick').one(symbol).one(sod);
+    var messages = Restangular.one('stocks').one(symbol).one(sod);
 
-    ticks.get().then(function (response) {
+    messages.get().then(function (response) {
       // we expect that each charted item is present in the incoming payload...
       _.forEach($scope.data, function(item) {
-        $scope.data[item.position].values = _.sortBy(_.map(response.ticks, _.curry(convertTicks)(item.key)), 'x');
+        $scope.data[item.position].values = _.sortBy(_.map(response.stocks, _.curry(convertMessages)(item.key)), 'x');
       });
     });
 
@@ -111,13 +111,13 @@ angular.module('atacamaApp')
         transitionDuration: 500,
         xAxis: {
           axisLabel: 'Dates',
-          tickFormat: function (d) {
+          messageFormat: function (d) {
             return d3.time.format('%X')(new Date(d));
           },
         },
         yAxis: {
           axisLabel: 'Stock Price',
-          tickFormat: function (d, i) {
+          messageFormat: function (d, i) {
             return '$' + d3.format(',.1f')(d);
           }
         }
