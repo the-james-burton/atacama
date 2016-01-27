7b6588d3677cd063e807a36336b05accf8e71c11
the-james-burton <the.james.burton@gmail.com>
2016-01-27 18:25:30 +0000

    added controller for new ohlc widget

---
+++ app/scripts/controllers/widgets/ohlcController.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name atacamaApp.controller:WidgetCtrl
+ * @description
+ * # WidgetCtrl
+ * Controller of the atacamaApp
+ */
+angular.module('atacamaApp')
+    .controller('OhlcWidgetCtrl', function($scope) {
+        $scope.blah1 = {
+            name: "woo! yay!"
+        }
+    });
