a1ef3c78c12c4796b40c16193e110f51c69af59a
the-james-burton <the.james.burton@gmail.com>
2016-02-10 18:26:42 +0000

    [Style Y024] Named vs Anonymous Functions

--- app/scripts/directives/widgets/ohlcDirective.js
+++ app/scripts/directives/widgets/ohlcDirective.js
@@ -1,16 +1,21 @@
-﻿'use strict';
+(function() {
 
-angular.module('atacamaApp').directive('atacamaOhlc',
-    ['dataService',
-    function (dataService) {
-        return {
-            templateUrl: 'views/widgets/ohlc.html',
-            link: function (scope, element, attrs) {
-                //set to null by default so images will not try to load until the data is returned
-                scope.selectedLocation = null;
-                scope.isLoaded = false;
-                scope.hasError = false;
-                scope.offsetParent = element.offsetParent();
-            }
-        };
-    }]);
+  'use strict';
+
+  angular.module('atacamaApp').directive('atacamaOhlc', ohlcDirective);
+
+  function ohlcDirective(dataService) {
+      return {
+          templateUrl: 'views/widgets/ohlc.html',
+          link: linkFunc
+      };
+  }
+
+  function linkFunc (scope, element, attrs) {
+      //set to null by default so images will not try to load until the data is returned
+      scope.selectedLocation = null;
+      scope.isLoaded = false;
+      scope.hasError = false;
+      scope.offsetParent = element.offsetParent();
+  }
+})();
