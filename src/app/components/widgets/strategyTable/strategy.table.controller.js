36d5bd160ff79f15aae8ac32bd68233b0ddf4290
the-james-burton <the.james.burton@gmail.com>
2016-09-28 18:11:29 +0100

    issue #16 refactored strategy table

--- src/app/components/widgets/strategyTable/strategy.table.controller.js
+++ src/app/components/widgets/strategyTable/strategy.table.controller.js
@@ -4,7 +4,7 @@
 
   /**
    * @ngdoc function
-   * @name atacamaApp.controller:StrategyTableController
+   * @name atacamaApp.controller:WidgetController
    * @description
    * # WidgetController
    * Controller of the atacamaApp
@@ -17,194 +17,89 @@
     elasticsearchService, chartService, turbineService, utilService, widgetService) {
     var vm = this;
 
-    // adjustments to make the chart fit better in the widget...
-    var adjustX = -35;
-    var adjustY = -65;
 
     // TODO select market in UI
     var market = 'FTSE100';
 
     var topic = '';
-    var esError = '';
-    var stompError = '';
+    var stompSubscription = '';
+
+    $log.info(widgetService.adjustX);
 
     vm.Status = _.keyBy(['WAITING', 'LOADING', 'LOADED', 'ERROR'], _.identity);
 
     vm.status = vm.Status.WAITING;
 
-    // vm.selectedSymbol = "";
-    vm.selectedSymbol = $scope.item.symbol;
-
-    vm.strategies = {};
     vm.actions = {};
 
-    $scope.$watchGroup(['vm.selectedSymbol', 'vm.dateFrom'], function (newValues, oldValues) {
-      if (!vm.selectedSymbol || !vm.dateFrom) {
-        return;
-      }
-      $scope.item.symbol = vm.selectedSymbol;
-      $scope.item.dateFrom = vm.dateFrom;
-      vm.dateFrom.setHours(0);
-      vm.dateFrom.setMinutes(0);
-      vm.dateFrom.setSeconds(0);
-      $log.log('detected updates: old:{0}, new:{1}'.format(
-        angular.toJson(oldValues), angular.toJson(newValues)));
-      doTable($scope.item);
-    }, false);
+    // load the saved values...
+    vm.symbol = $scope.item.symbol;
+    vm.dateFrom = new Date($scope.item.dateFrom); // marshall saved string into Date object
 
-    // set the size of the chart as the widget is resized...
-    // $scope.$on('gridster-item-resized', function (item, gridsterWidget) {
-    //   $log.debug('gridster-item-resized {0}x{1}'.format(
-    //     gridsterWidget.getElementSizeX(), gridsterWidget.getElementSizeY()));
-    //   vm.options.chart.width = gridsterWidget.getElementSizeX() + adjustX;
-    //   vm.options.chart.height = gridsterWidget.getElementSizeY() + adjustY;
-    // });
+    widgetService.startWatches(['symbol', 'dateFrom'], doTable, $scope);
 
-    // $scope.$watch('vm.values', function (newValues) {
-    //   if (newValues) {
-    //     vm.data = [{
-    //       key: vm.selectedSymbol,
-    //       values: newValues
-    //     }];
-    //   }
-    // }, true);
-
-    // ---------------------- datepicker stuff below ------------------
-    vm.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    vm.format = vm.formats[0];
-    vm.altInputFormats = ['M!/d!/yyyy'];
-
-    vm.dateOptions = {
-      formatYear: 'yy',
-      startingDay: 1
-    };
-
-    vm.popup1 = {
-      opened: false
-    };
-
-    // Disable weekend selection
-    vm.isDisabled = function(date, mode) {
-      return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-    };
-
-    vm.today = function() {
-      var dateFrom = new Date();
-      dateFrom.setHours(0);
-      dateFrom.setMinutes(0);
-      dateFrom.setSeconds(0);
-      vm.dateFrom = dateFrom;
-    };
-
-    vm.clear = function() {
-      vm.dateFrom = null;
-    };
-
-    vm.open1 = function() {
-      vm.popup1.opened = true;
-    };
-
-    vm.today();
-
-    // update chart on date change...
-    // $scope.$watch('vm.dateFrom', function (newValues) {
-    //   if (newValues) {
-    //     vm.dateFrom.setHours(0);
-    //     vm.dateFrom.setMinutes(0);
-    //     vm.dateFrom.setSeconds(0);
-    //     $log.log('detected date update: ', vm.dateFrom);
-    //     doTable($scope.item);
-    //   }
-    // }, true);
-
-    function fetchStrategies() {
-      turbineService.strategies().then(function (response) {
-        vm.strategies = response.strategies;
-        $log.info('available strategies : {0}'.format(angular.toJson(vm.strategies)));
-      }, function (err) {
-        esError = 'unable to load strategies: {0}'.format(err.message);
-        $log.error(esError);
-      });
+    // ---------------------------------------------------
+    function onError(message, err) {
+      vm.status = vm.Status.ERROR;
+      var error = '{0}: {1}:{2}'.format(message, vm.symbol, err.message);
+      $log.error(error);
+      return error;
     }
 
-    function fetchHistoricDataFromES(from) {
+    // ---------------------------------------------------
+    function loadElasticsearchDataIntoChart(results) {
+      vm.actions = results;
+    };
 
-      // TODO fetch all strategies..?
-      var promise = elasticsearchService.getStrategyActionsAfter(market, vm.selectedSymbol, "SMAStrategy", from);
-
-      promise.then(function (response) {
-        utilService.traceLog($scope.item, "elasticsearch");
-        vm.actions = elasticsearchService.parseResults(response);
-      }, function (err) {
-        esError = 'unable to load data: {0}:{1}'.format(vm.selectedSymbol, err.message);
-        $log.error(esError);
-      });
-    }
-
-    function subscribeToStompUpdates() {
-      // TODO subscribed to all strategies..?
-      topic = "/topic/strategies.{0}.{1}.*".format(market, vm.selectedSymbol);
-      //ngstomp.subscribe(topic, onMessage, {}, $scope);
-
-      try {
-        ngstomp
-          .subscribeTo(topic)
-          .callback(onMessage)
-          .withBodyInJson()
-          .bindTo($scope)
-          .connect();
-        // throw new Error("unable to subscribe to topic: " + topic);
-      } catch (err) {
-        stompError = 'unable to connect to: {0}:{1}'.format(topic, err.message);
-        $log.error(stompError);
-      }
-    }
-
-    function onMessage(message) {
-      // TODO avoid $scope?
+    // ---------------------------------------------------
+    function pushNewDataFromStompIntoChart(message) {
+      // don't show 'none' actions...
       if (message.body.action === 'none') {
         return;
       }
+      // TODO avoid $scope?
       utilService.traceLog($scope.item, "rabbit");
       vm.actions.push(message.body);
-      $scope.$apply();
+      // $scope.$apply();
+    };
+
+    // -----------------------------------------------------
+    // TODO due to ES client use of promises, we need to use callbacks...
+    function fetchHistoricDataFromElasticsearch(market, selectedSymbol, fromMilliseconds, successCallback, errorCallback) {
+      // TODO fetch all strategies..?
+      var promise = elasticsearchService.getStrategyActionsAfter(market, vm.symbol, "SMAStrategy", fromMilliseconds);
+      promise.then(function (response) {
+        var results = elasticsearchService.parseResults(response)
+        successCallback(results);
+      }, function (err) {
+        errorCallback('unable to load ES data', err)
+      });
     }
 
-
+    // ---------------------------------------------------
     function doTable(item) {
-      $log.debug("strategy.table.controller.js::doChart");
-      if (!vm.selectedSymbol) {
+      if (!vm.symbol) {
         return;
       }
+
+      $log.debug("strategy.table.controller.js::doChart");
       vm.status = vm.Status.LOADING;
-      // $scope.item = item;
-      item.name = vm.selectedSymbol;
+      // vm.chart = widgetService.emptyChart();
       widgetService.unsubscribeTopic(topic);
 
+      // var sod = moment(0, "HH").format("x");
+      var fromMilliseconds = moment(vm.dateFrom).format("x");
       utilService.traceLog(item, "elasticsearch");
 
-      // var sod = moment(0, "HH").format("x");
-      var from = moment(vm.dateFrom).format("x");
+      // NOTE can't use return value because ES client uses promises...
+      fetchHistoricDataFromElasticsearch(market, vm.symbol, fromMilliseconds, loadElasticsearchDataIntoChart, onError);
 
-      // get list of all available strategies from the turbine...
-      fetchStrategies();
-
-      // update vm.strategies with historic data from ES...
-      fetchHistoricDataFromES(from);
-
-      subscribeToStompUpdates();
+      topic = widgetService.strategyTopicRoot + '.' +  market + '.' + vm.symbol + '.*';
+      stompSubscription = widgetService.subscribeToStompUpdates($scope, topic, pushNewDataFromStompIntoChart, onError);
 
       vm.status = vm.Status.LOADED;
-      // vm.isLoaded = true;
-
-      // TODO catch all errors and set the property and message as required
-      if (esError !== '' || stompError !== '') {
-        vm.status = vm.Status.ERROR;
-        vm.hasError = true;
-      }
 
     }
-
 
   }
 
